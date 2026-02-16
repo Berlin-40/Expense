53233bcbdf0fb68c86b957711839eded45b0498f
kamel <fotsopaulin47@proton.me>
2026-02-16 16:48:42 +0100

    Feature: ajout de l'objet Expensive

---
+++ src/main/java/com/Expense/Model/Expense.java
@@ -0,0 +1,23 @@
+package com.Expense.Model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Expense {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    
+    private Long id;
+    private String label;
+    private Double amount;
+    private String category;
+    private Date date;
+}
